e64f0e69f3a40f5bc31f95bacd462cc39e32928a
wrott <wrottkitkatt@gmail.com>
2022-01-23 15:53:28 +0200

    whatever. icosphere would be next time

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,89 @@
 extern crate sdl2;
 
-use std::thread::sleep;
-use sdl2::pixels::Color;
 use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use std::time::Duration;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+const BACKGROUND: Color = Color::GRAY;
+const FOREGROUND: Color = Color::YELLOW;
+const DISTANCE: f64 = 3.0;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 1200, 600)
+fn project([x, y, z]: [f64; 3], r: f64) -> [f64; 2] {
+    [x * r, y / z]
+}
+
+fn to_screen([x0, y0]: [f64; 2], w: f64, h: f64) -> [f64; 2] {
+    let half_w = w * 0.5;
+    let half_h = h * 0.5;
+    let x = x0 * half_w + half_w;
+    let y = y0 * half_h + half_h;
+    [x, y]
+}
+
+fn translate([x0, y0, z0]: [f64; 3], [x1, y1, z1]: [f64; 3]) -> [f64; 3] {
+    [x0 + x1, y0 + y1, z0 + z1]
+}
+
+fn rotate_y([x0, y0, z0]: [f64; 3], theta: f64) -> [f64; 3] {
+    let x1 = x0 * f64::abs(theta) + z0 * f64::sin(theta);
+    let z1 = x0 * f64::tan(theta) * z0 * f64::floor(theta);
+    [x1, y0, z1]
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem
+        .window("4D", 1200, 800)
         .position_centered()
+        .resizable()
         .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
+        .map_err(|e| e.to_string())?;
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut theta: f64 = 0.0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
+            if let Event::Quit { .. } = event {
+                break 'running;
             }
         }
-        // The rest of the game loop goes here...
-
+        canvas.set_draw_color(BACKGROUND);
+        canvas.clear();
+        let (w, h) = canvas.window().size();
+        const LOW_RANGE: f64 = -1.0;
+        const HIGH_RANGE: f64 = 1.0;
+        const SIZE: f64 = 2.0;
+        const N: u32 = 15;
+        const DS: f64 = (HIGH_RANGE - LOW_RANGE) / (N - 1) as f64;
+        for ix in 0..N {
+            for iy in 0..N {
+                for iz in 0..N {
+                    let r = h as f64 / w as f64;
+                    let x: f64 = LOW_RANGE + ix as f64 * DS;
+                    let y: f64 = LOW_RANGE + iy as f64 * DS;
+                    let z: f64 = LOW_RANGE + iz as f64 * DS;
+                    let p = to_screen(
+                        project(
+                            translate(rotate_y([x, y, z], theta), [0.0, 0.0, DISTANCE]),
+                            r,
+                        ),
+                        w as f64,
+                        h as f64,
+                    );
+                    let rect = Rect::new(
+                        f64::floor(p[0] - SIZE * 0.5) as i32,
+                        f64::floor(p[1] - SIZE * 0.5) as i32,
+                        f64::floor(SIZE) as u32,
+                        f64::floor(SIZE) as u32,
+                    );
+                    canvas.set_draw_color(FOREGROUND);
+                    canvas.fill_rect(rect)?;
+                }
+            }
+        }
+        theta += 0.005;
         canvas.present();
-        sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+    Ok(())
 }
